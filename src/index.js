6e1d8f86127b7f027d8454f55b9354645d004444
hyeonuk-choo <hyeonuk97@naver.com>
2023-03-07 16:38:35 +0900

    style: Apply global style
    remove index.css
    edit index.html for google font
    Add GlobalStyle.js component to apply the font format at once.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { store } from './redux/config/configStore';
-import { Provider } from 'react-redux';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./redux/config/configStore";
+import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import GlobalStyle from "./style/GlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>
 );
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: 'Gowun Dodum', sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: none;
+}
+`;
+
+export default GlobalStyle;
